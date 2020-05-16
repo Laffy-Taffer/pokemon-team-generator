a46536b6e8f1fa92513e63827f42a1f34bbf7c67
Taffer <62994178+Laffy-Taffer@users.noreply.github.com>
2020-05-16 02:01:50 -0700

    Update teamgenerator.py

--- teamgenerator.py
+++ teamgenerator.py
@@ -1,5 +1,7 @@
-import csv, itertools
-with open('Pokemon Team Comps/types.csv', 'rt') as f:
+import csv, itertools, os, math
+from multiprocessing import Pool
+
+with open('types.csv', 'rt') as f:
     f.__next__()
     csv_reader = csv.reader(f)
     typelist = []
@@ -10,21 +12,67 @@ with open('Pokemon Team Comps/types.csv', 'rt') as f:
         matchup += [line[1:-1]]
 
 length = len(matchup)
-with open('Pokemon Team Comps/teams.csv', 'wt', newline='') as f:
-    csv_writer = csv.writer(f)
-    for team in itertools.combinations(range(length), 6):
-        weaknesses = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        send = True
+teamfact = math.factorial(5)
+new = False
+if not os.path.exists('output'):
+    os.makedirs('output')
+    new = True
+
+
+def teamcomp(r):
+    global length, typelist, matchup
+    comborange = length-r+1
+    combofact = math.factorial(comborange)
+    print("Starting team check", r, "Number of combinations:",
+          '{:,}'.format(int(combofact/(teamfact*math.factorial(comborange-5)))))
+    with open('output/teams' + str(r) + '.csv', 'wt', newline='') as c:
+        writer = csv.writer(c)
+        for team in itertools.combinations(range(r+1, length), 5):
+            team = tuple([r] + [t for t in team])
+            weaknesses = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            send = True
+            for element in range(18):
+                for pokemon in team:
+                    if float(matchup[pokemon][element]) > 1:
+                        weaknesses[element] += 1
+                        if weaknesses[element] > 2:
+                            send = False
+                            break
+                if not send:
+                    break
+
+            if send:
+                output = ''.join([typelist[a] for a in team])
+                writer.writerows([[output]])
+    os.rename('output/teams' + str(r) + '.csv',
+              'output/teams' + str(r) + 'done.csv')
+    print("                       Finished with team", r)
+
+
+def pool_handler(n):
+    p = Pool(n)
+    starter = reversed(range(start))
+    p.map(teamcomp, starter)
+
+
+if __name__ == '__main__':
+
+    x = os.listdir("output")
+    start = highest = 0
+    if not new:
+        everything = [a.replace('teams', '') for a in x]
+        highest = max([int(a.replace('teams', '').replace('done', '').replace('.csv', '')) for a in x])
+        nextone = max([int(a.replace('teams', '').replace('.csv', '')) for a in x if 'done' not in a])
+        start = input("Input value to count down from\nHighest value: " + str(highest) +
+                      "  Highest uncalculated value: " + str(nextone) + "\n")
+    poolcount = input("How many cores do you want to use?\n")
+
+    try:
+        start = int(start) + 1
+        poolcount = int(poolcount)
+        if start < 1 or start > highest or poolcount < 1:
+            quit()
+    except ValueError:
+        quit()
         
-        for element in range(18):
-            for pokemon in team:
-                if float(matchup[pokemon][element]) > 1:
-                    weaknesses[element] += 1
-                    if weaknesses[element] > 2:
-                        send = False
-                        break
-            if not send:
-                break
-        if send:
-            output = ''.join([typelist[a] for a in team])
-            csv_writer.writerows([[output]])
+    pool_handler(poolcount)
