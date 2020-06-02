7d56045953d2149adc6f628aac24cadeea34e25e
Taffer <62994178+Laffy-Taffer@users.noreply.github.com>
2020-06-02 00:03:29 -0700

    Update Team Generator.py

--- Team Generator.py
+++ Team Generator.py
@@ -10,25 +10,18 @@ with open('all types sorted.csv', 'rt') as f:  # Reads type matchup list and ass
         typelist += [line[0]]
         matchup += [line[1:]]
 
-for outputnum in range(len(typelist)):  # Creates output folders and files
-    if not os.path.exists('output/'+str(outputnum)):
-        os.makedirs('output/'+str(outputnum))
-for x in itertools.combinations(range(len(matchup)-4), 2):
-    if not os.path.exists('output/' + str(x[0]) + '/' + str(x[1]) + '.csv'):
-        with open('output/' + str(x[0]) + '/' + str(x[1]) + '.csv', 'w'):
-            pass
 
-teamlist = list(itertools.combinations(range(170), 2))
+teamlist = list(itertools.combinations(range(len(matchup)-4), 2))
 teamcount = len(teamlist)
 
 
 def matchupcheck(team):  # Checks if a team has no shared weaknesses
     for element in range(18):
-        x = 0
+        i = 0
         for pokemon in team:
             if float(pokemon[element]) > 1:
-                x += 1
-            if x > 1:
+                i += 1
+            if i > 1:
                 return False
     return True
 
@@ -40,7 +33,7 @@ def teamcomp(types):  # Generate team combinations, runs matchupcheck, writes te
         return
     print("Starting pass on " + str(types))
     savecount = 0
-    
+
     with open('output/' + str(types[0]) + '/' + str(types[1]) + '.csv', 'wt', newline='') as c:
         writer = csv.writer(c)
         match1 = matchup[types[0]]
@@ -50,7 +43,7 @@ def teamcomp(types):  # Generate team combinations, runs matchupcheck, writes te
             if matchupcheck(squad):
                 savecount += 1
                 writer.writerow([''.join([typelist[a] for a in team])])
-                
+
     os.rename('output/' + str(types[0]) + '/' + str(types[1]) + '.csv',
               'output/' + str(types[0]) + '/' + str(types[1]) + ' done.csv')
     teamlist.remove(types)
@@ -61,8 +54,13 @@ def teamcomp(types):  # Generate team combinations, runs matchupcheck, writes te
 
 def poolhandler(wcount):
     p = Pool(wcount)
-    p.map(teamcomp, list(itertools.combinations(range(len(matchup)-4), 2)))
+    p.map(teamcomp, reversed(list(itertools.combinations(range(len(matchup)-4), 2))))
 
 
 if __name__ == '__main__':
+
+    for outputnum in range(len(typelist) - 5):  # Creates output folders and files
+        if not os.path.exists('output/' + str(outputnum)):
+            os.makedirs('output/' + str(outputnum))
+
     poolhandler(int(input('Worker count:')))
